acaf48c70c00a4c54f2d275ba561a703ddbc4ab3
Damien Deprez <damien.deprez@student.uclouvain.be>
2017-03-03 11:36:27 +0100

    Ajout du tir (annimation de la balle)

--- MTL2-Baseline-Quartus-Project/software/Projet-uC-OSII/hello_ucosii.c
+++ MTL2-Baseline-Quartus-Project/software/Projet-uC-OSII/hello_ucosii.c
@@ -29,6 +29,8 @@
 
 
 #include <stdio.h>
+#include <math.h>
+
 #include "includes.h"
 #include "system.h"
 
@@ -42,6 +44,11 @@ OS_STK    task2_stk[TASK_STACKSIZE];
 #define TASK1_PRIORITY      1
 #define TASK2_PRIORITY      2
 
+/* Definition of the mailboxes */
+OS_EVENT *MailBox1;
+OS_EVENT *MailBox2;
+OS_EVENT *MailBox3;
+
 /* Prints "Hello World" and sleeps for three seconds */
 void task1(void* pdata)
 {
@@ -49,8 +56,10 @@ void task1(void* pdata)
 	int count_old = 0;
 	int count = 0;
 
-	int x1_gesture_start, x2_gesture_start, x2_gesture_stop ;
-	int y1_geture_start, y2_gesture_start, y2_gesture_stop;
+	int x1_gesture_start, x1_gesture_stop, x2_gesture_start, x2_gesture_stop ;
+	int y1_gesture_start, y1_gesture_stop, y2_gesture_start, y2_gesture_stop;
+
+	int gesture_detected = 0;
 
 	while (1){
 			count_old = count;
@@ -65,7 +74,7 @@ void task1(void* pdata)
 			{
 				printf("start gesture\n");
 				x1_gesture_start = pos1 & 0x0003FF;
-				y1_geture_start = pos1 >> 10;
+				y1_gesture_start = pos1 >> 10;
 
 				x2_gesture_start = pos2 & 0x0003FF;
 				y2_gesture_start = pos2 >> 10;
@@ -73,21 +82,39 @@ void task1(void* pdata)
 			if(count_old == 2 && count == 1) // si on pass de 2 à 1 doigt
 			{
 				printf("stop gesture\n");
+				x1_gesture_stop = pos1 & 0x0003FF;
+				y1_gesture_stop = pos1 >> 10;
+
 				x2_gesture_stop = pos2 & 0x0003FF;
 				y2_gesture_stop = pos2 >> 10;
+				gesture_detected =     (x1_gesture_start -30 <= x1_gesture_stop && x1_gesture_stop <= x1_gesture_start + 30)
+									&& (y1_gesture_start -30 <= y1_gesture_stop && y1_gesture_stop <= y1_gesture_start + 30);
 			}
 
 			//m =(y2_gesture_stop-y1_geture_start)/(x2_gesture_stop-x1_gesture_start);
 	        //p = (y1_geture_start-(m*x1_gesture_start));
-
-			*(MTL_controller + 1) = (y1_geture_start << 10) + x1_gesture_start;
+			*(MTL_controller + 1) = (y1_gesture_start << 10) + x1_gesture_start;
 			*(MTL_controller + 2) = (y2_gesture_start << 10) + x2_gesture_start;
 			*(MTL_controller + 3) = (y2_gesture_stop << 10) + x2_gesture_stop;
 
-			int x_dir = (x2_gesture_stop - x1_gesture_start);
-			int y_dir = (y2_gesture_stop - y1_geture_start);
+			if(gesture_detected){
+				int x_dir = (x2_gesture_stop - x1_gesture_start);
+				int y_dir = (y2_gesture_stop - y1_gesture_start);
 
-			printf("tir : (%d, %d)\n", x_dir, y_dir);
+				double l = sqrt(x_dir*x_dir + y_dir*y_dir);
+
+				double vector_x = x_dir/l;
+				double vector_y = y_dir/l;
+
+				double speed = 200;
+
+				printf("tir : (%.2f, %.2f)\n", vector_x, vector_y);
+
+				OSMboxPost(MailBox1, &vector_x);
+				OSMboxPost(MailBox2, &vector_y);
+				OSMboxPost(MailBox3, &speed);
+				gesture_detected = 0;
+			}
 
 		OSTimeDlyHMSM(0,0,0,10);
 	}
@@ -96,38 +123,106 @@ void task1(void* pdata)
 void task2 (void *pdata)
 {
 	volatile int * MTL_controller = (int *) MTL_IP_BASE;
+
+	int x4 = 64;
+	int y4 = 64;
+	int x5 = 0;//128;
+	int y5 = 0;//64;
+	int x6 = 0;//196;
+	int y6 = 0;//64;
+	int x7 = 0;//256;
+	int y7 = 0;//64;
+	int x8 = 0;//320;
+	int y8 = 0;//64;
+	int x9 = 0;//384;
+	int y9 = 0;//64;
+	int x10 = 0;//64;
+	int y10 = 0;//128;
+
+	*(MTL_controller + 4) = (y4 << 10) + x4;
+	*(MTL_controller + 5) = (y5 << 10) + x5;
+	*(MTL_controller + 6) = (y6 << 10) + x6;
+	*(MTL_controller + 7) = (y7 << 10) + x7;
+	*(MTL_controller + 8) = (y8 << 10) + x8;
+	*(MTL_controller + 9) = (y9 << 10) + x9;
+	*(MTL_controller + 10) = (y10 << 10) + x10;
+
+	INT8U err;
+
+	int hBorder = 0;
+	int vBorder = 0;
+
+	int backX = 0;
+	int backY = 0;
+
+	int maxX = 800;
+	int maxY = 480;
+
+	int size = 32;
+	int border = 0;
+
+	double vector [2];
+	double speed;
+
+	double ball_x = 64, ball_y = 64;
+
 	while(1){
-		int x4 = 64;
-		int y4 = 64;
-		int x5 = 128;
-		int y5 = 64;
-		int x6 = 196;
-		int y6 = 64;
-		int x7 = 256;
-		int y7 = 64;
-		int x8 = 320;
-		int y8 = 64;
-		int x9 = 384;
-		int y9 = 64;
-		int x10 = 64;
-		int y10 = 128;
 
-		*(MTL_controller + 4) = (y4 << 10) + x4;
-		*(MTL_controller + 5) = (y5 << 10) + x5;
-		*(MTL_controller + 6) = (y6 << 10) + x6;
-		*(MTL_controller + 7) = (y7 << 10) + x7;
-		*(MTL_controller + 8) = (y8 << 10) + x8;
-		*(MTL_controller + 9) = (y9 << 10) + x9;
-		*(MTL_controller + 10) = (y10 << 10) + x10;
+		double *vector_x = OSMboxPend(MailBox1,0,&err);
+		double *vector_y = OSMboxPend(MailBox2,0,&err);
+		double *speed_msg = OSMboxPend(MailBox3,0,&err);
+		vector[0] = *vector_x;
+		vector[1] = *vector_y;
+		speed = *speed_msg;
+		int backX = vector[0] <0;
+		int backY = vector[1] <0;
 
-		OSTimeDlyHMSM(0,0,3,0);
+		vector[0] = fabs(vector[0]);
+		vector[1] = fabs(vector[1]);
+
+		printf("task 3 -> shoot ball 4 (%.2f, %.2f) %.2f\n", vector[0], vector[1], speed);
+
+		while (speed >= 0){
+			//printf("task 3 -> shoot ball 4 (%.2f, %.2f) %.2f\n", vector[0], vector[1], speed);
+
+			if(ball_x < border+size+hBorder + 1)
+				backX = 0; // avance
+			if(ball_x > (maxX+hBorder)-(border+size))
+				backX = 1; // recule
+			if(ball_y < border + size+vBorder+1)
+				backY = 0;
+			if(ball_y>(maxY+vBorder)-(border+size))
+				backY = 1;
+
+			OSTimeDlyHMSM(0,0,0,40);
+
+			if(!backX) ball_x+=vector[0]*(speed/10.0);
+			else ball_x-=vector[0]*(speed/10.0);
+
+			if(!backY) ball_y+=vector[1]*(speed/10.0);
+			else ball_y-=vector[1]*(speed/10.0);
+
+			//ball_x += vector[0] * (speed/10.0);
+			//ball_y += vector[1] * (speed/10.0);
+
+			*(MTL_controller + 4) = ((int)(ball_y) << 10) + (int)(ball_x);
+
+			speed = speed - 1;
+
+		}
 	}
 }
 
 /* The main function creates two task and starts multi-tasking */
 int main(void)
 {
-  
+  OSInit();
+
+
+	MailBox1 = OSMboxCreate(NULL);
+	MailBox2 = OSMboxCreate(NULL);
+	MailBox3 = OSMboxCreate(NULL);
+
   OSTaskCreateExt(task1,
                   NULL,
                   (void *)&task1_stk[TASK_STACKSIZE-1],
